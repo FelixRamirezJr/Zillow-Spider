1b9109c62fa0a0dde8ce4a18915400966f6b9bca
= <sk8withaguitar@gmail.com>
2019-03-02 21:51:15 -0800

    Improve scraping by visiting individual housing pages and extracting data there (methdo of looping through listing was causing data issues)

--- zillow_scraper.py
+++ zillow_scraper.py
@@ -24,6 +24,7 @@ class ZillowScraper(scrapy.Spider):
     handle_httpstatus_list = [500, 503, 504, 400, 408, 307, 403]
     #login_url = 'https://www.zillow.com/user/acct/login/'
 
+
     def start_requests(self):
         urls_to_scrape = []
         agent = user_agent_rotator.get_random_user_agent()
@@ -38,12 +39,48 @@ class ZillowScraper(scrapy.Spider):
             if url not in urls_to_scrape:
                 urls_to_scrape.append(url)
                 print('Sleeping...Zzzz')
-                time.sleep(5)
+                time.sleep(1)
                 print('Crawling: ' + url)
                 yield scrapy.Request(url,
                     callback=self.parse,
                     meta={'city': city, 'state': state, 'location': city + ' ' + state, 'page': 1},
                     headers={"User-Agent": agent})
+
+    def parse_house(self, response):
+        print("parsing house")
+        print(response.meta['location'])
+
+        # Let's get the price
+        price = response.xpath('//div[@class="price"]/span/text()').extract_first()
+        if price == None:
+            print("Couldn't find price with .price class attempting to find with ds-value")
+            price = response.xpath('//span[@class="ds-value"]/text()').extract_first()
+
+        price = price.replace('$', '').replace(',', '').strip()
+
+        print("===== FINAL PRICE ======")
+        print(price)
+
+        # Now let's get the details
+        beds = ''
+        bathrooms = ''
+        for text in response.xpath('//span/text()').extract():
+            if "beds" in text or text.isdigit():
+                print("Found beds")
+                beds = text.replace('beds', '').strip()
+            if "baths" in text or text.isdigit():
+                print("Found baths")
+                bathrooms = text.replace('baths', '').strip()
+            if len(beds) > 0 and len(bathrooms) > 0:
+                break
+
+        if len(price) > 0 and len(beds) > 0 and len(bathrooms) > 0:
+            print("==== Found some good data ====")
+            data = {'rooms': beds, 'description': '' ,'bathrooms': bathrooms, 'price': price, 'url': response.url, 'location': response.meta['location']}
+            print(data)
+            r = requests.post(url = send_data_url, data = data)
+            print(r.text)
+
 
 
     def parse(self, response):
@@ -55,44 +92,56 @@ class ZillowScraper(scrapy.Spider):
         # Get the url first
         for link_dom in response.css('.zsg-photo-card-overlay-link'):
             url = link_dom.css('a::attr(href)').extract_first()
-            data = {'rooms': '', 'description': '' ,'bathrooms': '', 'price': '', 'url': '', 'location': ''}
-            data['location'] = response.meta['location']
-            data['url'] = "https://www.zillow.com" + url
-            all_data.append(data)
+            # data = {'rooms': '', 'description': '' ,'bathrooms': '', 'price': '', 'url': '', 'location': ''}
+            # data['location'] = response.meta['location']
+            # data['url'] = "https://www.zillow.com" + url
+            print('sleeping before trying to scrape' + url)
+            time.sleep(1)
+            yield scrapy.Request(zillow_url + url,
+                callback=self.parse_house,
+                meta=response.meta,
+                headers={"User-Agent": user_agent_rotator.get_random_user_agent()})
 
-        description = ""
-        for house in response.css('.zsg-photo-card-caption'):
-            data = all_data[index]
-            data["description"] = house.xpath('//span[@class="zsg-photo-card-status"]/text()').extract_first()
-            #description += status
-            data["price"] = house.xpath('//p[@class="zsg-photo-card-spec"]/span[@class="zsg-photo-card-price"]/text()').extract_first()
-            data['price'] = data['price'].replace('$', '').replace(',','').strip()
+            #all_data.append(data)
 
-            data["rooms"] = house.xpath('//p[@class="zsg-photo-card-spec"]/span[@class="zsg-photo-card-info"]/text()').extract_first()
-            data["rooms"] = data["rooms"].replace('bds', '').strip()
-
-            data["bathrooms"] = house.xpath('//p[@class="zsg-photo-card-spec"]/span[@class="zsg-photo-card-info"]/text()').extract()[1]
-            data["bathrooms"] = data["bathrooms"].replace('ba', '').strip()
-            data['description'] = data["description"] + " URL: " + response.url + " page: " + str(response.meta['page'])
-            #print house.xpath('//p[@class="zsg-photo-card-spec"]/text()')
-            print("========== DATA ==========")
-            print(data)
-            index = index + 1
-            r = requests.post(url = send_data_url, data = data)
-            print(r.text)
-
-        print("About to try and get the next page")
-        for next in response.css('.zsg-pagination-next'):
-            print("About to try css get for zsg pagination")
-            next_page = zillow_url + next.xpath('a/@href').extract_first()
-            if next_page != None:
-                response.meta['page'] = response.meta['page'] + 1
-                print("Sleeping and going to the next page")
-                print(response.meta['page'])
-                print(next_page)
-                time.sleep(5)
-
-                yield scrapy.Request(next_page,
-                    callback=self.parse,
-                    meta=response.meta,
-                    headers={"User-Agent": user_agent_rotator.get_random_user_agent()})
+        # description = ""
+        # for house in response.css('.zsg-photo-card-caption'):
+        #     print("This is index at the start:")
+        #     print(index)
+        #     data = all_data[index]
+        #     data["description"] = house.xpath('//span[@class="zsg-photo-card-status"]/text()').extract()[index]
+        #     #description += status
+        #     data["price"] = house.xpath('//p[@class="zsg-photo-card-spec"]/span[@class="zsg-photo-card-price"]/text()').extract()[index]
+        #     data['price'] = data['price'].replace('$', '').replace(',','').strip()
+        #
+        #     room_data = house.xpath('//p[@class="zsg-photo-card-spec"]/span[@class="zsg-photo-card-info"]/text()').extract()[index]
+        #
+        #     data["rooms"] = room_data[0]
+        #     data["rooms"] = data["rooms"].replace('bds', '').strip()
+        #
+        #     data["bathrooms"] = room_data[1]
+        #     data["bathrooms"] = data["bathrooms"].replace('ba', '').strip()
+        #
+        #     data['description'] = data["description"] + " URL: " + response.url + " page: " + str(response.meta['page'])
+        #     #print house.xpath('//p[@class="zsg-photo-card-spec"]/text()')
+        #     print("========== DATA ==========")
+        #     print(data)
+        #     index = index + 1
+        #     #r = requests.post(url = send_data_url, data = data)
+        #     #print(r.text)
+        #
+        # print("About to try and get the next page")
+        # for next in response.css('.zsg-pagination-next'):
+        #     print("About to try css get for zsg pagination")
+        #     next_page = zillow_url + next.xpath('a/@href').extract_first()
+        #     if next_page != None:
+        #         response.meta['page'] = response.meta['page'] + 1
+        #         print("Sleeping and going to the next page")
+        #         print(response.meta['page'])
+        #         print(next_page)
+        #         time.sleep(5)
+        #
+        #         yield scrapy.Request(next_page,
+        #             callback=self.parse,
+        #             meta=response.meta,
+        #             headers={"User-Agent": user_agent_rotator.get_random_user_agent()})
